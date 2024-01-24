8c552df0743cf5c9acc8948488641d9310cb6035
Jove <jovebeckett2@gmail.com>
2024-01-23 21:19:38 -0800

    nicer repl experience with rustyline

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,32 @@ mod user;
 
 fn main() {
     let state = builtin::global_scope();
+    let mut rl = rustyline::DefaultEditor::new().unwrap();
+
     loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(input) => {
+                if input.trim().eq_ignore_ascii_case("quit") {
+                    break;
+                }
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+                rl.add_history_entry(input.as_str()).unwrap();
 
-        if input.trim().eq_ignore_ascii_case("quit") {
-            break;
-        }
-        let result = interpreter::eval(
-            parser::parse(lexer::Lexer::load(&input).tokens),
-            state.clone(),
-        );
+                let result = interpreter::eval(
+                    parser::parse(lexer::Lexer::load(&input).tokens),
+                    state.clone(),
+                );
 
-        if let interpreter::Type::Void = result {
-        } else {
-            println!("{}", result);
+                if let interpreter::Type::Void = result {
+                } else {
+                    println!("{}", result);
+                }
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
         }
     }
 }
